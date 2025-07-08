aea92d9f8a879348f4a9a21cc5a2e46142487047
VIN <166836664+xxtitled@users.noreply.github.com>
2025-07-08 21:49:11 +0900

    Add files via upload

--- Software/HV2_Upper_setting/HV2_Upper_setting.ino
+++ Software/HV2_Upper_setting/HV2_Upper_setting.ino
@@ -15,34 +15,37 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // [] 그룹에 묶어서 제어하는 다리 수
 // 즉 A에서는 Left Leg 한 개, B에서는 Right Leg 한 개 --> LEG_SIZE = 1
-const uint8_t LEG_SIZE = 1;
+const uint8_t LOWER_SIZE = 1;
 
 // A: Left Leg (골반, 힙, 무릎, 발목)
-const uint8_t PELVIS_A[LEG_SIZE] = {3};
-const uint8_t HIP_A[LEG_SIZE] = {2};
-const uint8_t KNEE_A[LEG_SIZE] = {1};
-const uint8_t ANKLE_A[LEG_SIZE] = {0};
+const uint8_t PELVIS_A[LOWER_SIZE] = {3};
+const uint8_t HIP_A[LOWER_SIZE] = {2};
+const uint8_t KNEE_A[LOWER_SIZE] = {1};
+const uint8_t ANKLE_A[LOWER_SIZE] = {0};
 
 // B: Right Leg (골반, 힙, 무릎, 발목)
-const uint8_t PELVIS_B[LEG_SIZE] = {7};
-const uint8_t HIP_B[LEG_SIZE] = {6};
-const uint8_t KNEE_B[LEG_SIZE] = {5};
-const uint8_t ANKLE_B[LEG_SIZE] = {4};
+const uint8_t PELVIS_B[LOWER_SIZE] = {7};
+const uint8_t HIP_B[LOWER_SIZE] = {6};
+const uint8_t KNEE_B[LOWER_SIZE] = {5};
+const uint8_t ANKLE_B[LOWER_SIZE] = {4};
 
 /* ------------- UPPER ------------- */
 
-// [] 그룹에 묶어서 제어하는 팔 수
-const uint8_t ARM_SIZE = 1;
+// [] 그룹에 묶어서 제어하는 팔 수 or 허리 
+const uint8_t UPPER_SIZE = 1;
 
 // C: Left Arm (가슴, 어깨, 팔꿈치)
-const uint8_t CHEST_C[ARM_SIZE] = {10};
-const uint8_t SHOULDER_C[ARM_SIZE] = {9};
-const uint8_t ELBOW_C[ARM_SIZE] = {8};
+const uint8_t CHEST_C[UPPER_SIZE] = {10};
+const uint8_t SHOULDER_C[UPPER_SIZE] = {9};
+const uint8_t ELBOW_C[UPPER_SIZE] = {8};
 
 // D: Right Arm (가슴, 어깨, 팔꿈치)
-const uint8_t CHEST_D[ARM_SIZE] = {13};
-const uint8_t SHOULDER_D[ARM_SIZE] = {12};
-const uint8_t ELBOW_D[ARM_SIZE] = {11};
+const uint8_t CHEST_D[UPPER_SIZE] = {13};
+const uint8_t SHOULDER_D[UPPER_SIZE] = {12};
+const uint8_t ELBOW_D[UPPER_SIZE] = {11};
+
+// E: WAIST (허리)
+const uint8_t WAIST_E[UPPER_SIZE] = {14};
 
 // PWM 서보 각도 설정
 void setAngle(uint8_t ch, float angle) {
@@ -52,13 +55,13 @@ void setAngle(uint8_t ch, float angle) {
 }
 
 // ============= LOWER ============= 
-void setGroupA(float pelvis, float hip, float knee, float ankle) {
+void setGroupA(float pelvis, float hip, float knee, float ankle) {    // L
   setAngle(PELVIS_A[0], pelvis);
   setAngle(HIP_A[0], hip);
   setAngle(KNEE_A[0], knee);
   setAngle(ANKLE_A[0], ankle);
 }
-void setGroupB(float pelvis, float hip, float knee, float ankle) {
+void setGroupB(float pelvis, float hip, float knee, float ankle) {    // R
   setAngle(PELVIS_B[0], pelvis);
   setAngle(HIP_B[0], hip);
   setAngle(KNEE_B[0], knee);
@@ -66,15 +69,18 @@ void setGroupB(float pelvis, float hip, float knee, float ankle) {
 }
 
 // ============= UPPER ============= 
-void setGroupC(float chest, float shoulder, float elbow) {
+void setGroupC(float chest, float shoulder, float elbow) {    // L
   setAngle(CHEST_C[0], chest);
   setAngle(SHOULDER_C[0], shoulder);
   setAngle(ELBOW_C[0], elbow);
 }
-void setGroupD(float chest, float shoulder, float elbow) {
+void setGroupD(float chest, float shoulder, float elbow) {    // R
   setAngle(CHEST_D[0], chest);
   setAngle(SHOULDER_D[0], shoulder);
   setAngle(ELBOW_D[0], elbow);
+}
+void setGroupE(float waist) {   // Core
+  setAngle(WAIST_E[0], waist);
 }
 
 void setup() {
@@ -92,6 +98,7 @@ void setup() {
   setGroupB(90, 90, 90, 90);
   setGroupC(90, 90, 90);
   setGroupD(90, 90, 90);
+  setGroupE(90);
 }
 
 void loop() {
@@ -147,18 +154,33 @@ void loop() {
 
     /* ------------- UPPER ------------- */
 
-    case 'd': case 'D': // --> 펀치
-      setGroupC(, , );   // L
-      setGroupD(, , );   // R
+    case 'd': case 'D': // --> 팔 X축과 평행하게 펴기
+      setGroupC(180, 90, 80);  // L
+      setGroupD(0, 90, 100);   // R
+      setGroupE(90);
       Serial.println("Preset D applied");
       break;
 
-    case '': case '': // --> 
-      setGroupC(, , );   // L
-      setGroupD(, , );   // R
+    case 'f': case 'F': // --> 팔 Y축과 평행하게 펴기
+      setGroupC(90, 180, 90);  // L
+      setGroupD(90, 0, 90);    // R
+      setGroupE(90);
       Serial.println("Preset  applied");
       break;
 
+    case 'c': case 'C': // --> 왼손 잽
+      setGroupC(180, 90, 80);  // L
+      setGroupD(90, 90, 90);   // R
+      setGroupE(90);
+      Serial.println("Preset D applied");
+      break;
+
+    case 'v': case 'V': // --> 오른손 잽
+      setGroupC(90, 90, 90);  // L
+      setGroupD(0, 90, 100);   // R
+      setGroupE(90);   //
+      Serial.println("Preset  applied");
+      break;
 
     /* -------------- 각도 초기화 -------------- */
 
@@ -167,6 +189,7 @@ void loop() {
       setGroupB(90, 90, 90, 90);
       setGroupC(90, 90, 90);
       setGroupD(90, 90, 90);
+      setGroupE(90);
       Serial.println("Preset 1 applied");
       break;
 
